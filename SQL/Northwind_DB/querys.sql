93ae68ea7568d5420e2b3b6896cd59574d8c3ad7
William Andres Ramirez Jimenez <frivandres2011@hotmail.com>
2025-03-22 23:01:06 -0500

    WR: Update.

--- SQL/Northwind_DB/querys.sql
+++ SQL/Northwind_DB/querys.sql
@@ -15,7 +15,7 @@
 -- select * from Products order by random();
 -- "distinct" Sirve para mostrar valores sin repetir.
 -- select distinct ProductName from Products;
--- "where" Sirve para poner condiciones y deacuerdo a la condición muestra data o realiza determinada acción.
+-- "where" Sirve para poner condiciones y deacuerdo a la condición muestra data o realiza determinada acción, para filtrar registros.
 -- select * from Products where ProductID == 14;
 -- "update" Sirve para hacer cambios en los registros de las tables.
 -- update users set lastname = 'Perez' where id_users == 2;
@@ -54,8 +54,8 @@ limit 3;
 -- select * from Products where CategoryID != 1;
 -- == -> De igualda.
 -- select * from Products where CategoryID == 1;
--- = -> De asignación, para el caso de los "update" con el "set".
--- update Products set ProductName = 'ProductName' where ProductID == 1;
+-- = -> De asignación o de comparación de igualda, para el caso de los "update" con el "set" y de comparación en el caso de los "where".
+-- update Products set ProductName = 'ProductName' where ProductID = 1;
 -- < -> Menor que.
 -- select * from Products where Price < 100;
 -- > -> Mayor que.
@@ -64,4 +64,60 @@ limit 3;
 -- select * from Products where Price <= 40;
 -- >= -> Mayor igual que.
 -- select * from Products where Price >= 40;
-select * from Products where CategoryID != 1;
+-- "between" Sirve para selecionar data en un rango especifico incluido los datos limites,
+-- el valor uno debe ser menor al valor dos.
+-- select * from Products where Price between 20 and 40;
+-- "like" Sirve para hacer filtros, similar a un igual.
+-- select * from Products where ProductName like 'Tofu';
+-- "%" Sirve para indicar que busque por el valor que comienza con "value%".
+-- select * from Products where ProductName like 'A%';
+-- "%" Sirve para indicar que busque por el valor que termina con "%value".
+-- select * from Products where ProductName like '%a';
+-- "%" Sirve para indicar que busque por el valor que contenga "%value%".
+-- select * from Products where ProductName like '%r%';
+-- "_" Sirve para hacer referencia a un caracter.
+-- Que comiencen con "C" y terminen con "s" y los "_" indican los caracteres en medio.
+-- select * from Products where ProductName like 'C___s';
+-- Que comiencen con "Cha" y los "_" indican los caracteres al final.
+-- select * from Products where ProductName like 'Cha__';
+-- Que termina con "ng" y los "_" indican los caracteres al inicio.
+-- select * from Products where ProductName like '___ng';
+-- El "%" y el "_" se pueden mesclar.
+-- select * from Products where ProductName like '%_ng';
+-- select * from Products where ProductName like 'Chef_%';
+-- select * from Products where ProductName like '%Chef_%';
+-- "is null" Sirve para validar si el value es nulo y lo devuelve.
+-- select * from Products where ProductName is null;
+-- "is not null" Sirve para validar si el value no es nulo y lo devuelve.
+-- select * from Products where ProductName is not null;
+-- Operador logico "in" es un tipo de "or" pero con tupla,
+-- "(campo=value)" "in" "(value, value, value, value, value)".
+-- select * from Products where SupplierID in (3, 4, 5, 6);
+-- "not in" para negar.
+-- select * from Products where SupplierID not in (3, 4, 5, 6);
+-- Funciones de agregación:
+-- count() -> Sirve para contar la cantidad de values en un campo o columna.
+-- select count(ProductName) as cantidad_products from Products;
+-- sum() -> Sirve para sumar los valores.
+-- select sum(Price) as suma_total from Products;
+-- avg() -> Sirve para devolver el promedio.
+-- select avg(Price) as promedio from Products;
+-- round(value, decimales) -> Sirve para redondear un valor.
+-- select round(avg(Price), 2) as redondear_promedio from Products;
+-- min() -> Sirve para devolver el valor minimo.
+-- select min(Price) as price_minimo from Products;
+-- max() -> Sirve para devolver el valor maximo.
+-- select max(Price) as price_maximo from Products;
+-- "group by" Sirve para agrupar varios registros según el valor de la columna.
+-- select SupplierID, round(avg(Price)) from Products group by SupplierID;
+-- "having" Sirve para filtrar grupos.
+-- select SupplierID, round(avg(Price)) as promedio_price from Products group by SupplierID having promedio_price > 40;
+-- Jerarquia de acciones:
+-- 1. select ... from ...
+-- 2. where ...
+-- 3. group by ...
+-- 4. having ...
+-- 5. order by ...
+-- 6. limit ...
+-- Subconsultas:
+select SupplierID, round(avg(Price)) as promedio_price from Products group by SupplierID having promedio_price > 40;
